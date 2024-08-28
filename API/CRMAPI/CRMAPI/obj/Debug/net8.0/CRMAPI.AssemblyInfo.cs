5d64f772b4699febf684e06ee4dc2743b9a40646
Zuzanna <80766251+Taidio1@users.noreply.github.com>
2024-08-28 10:11:03 +0200

    upd

--- API/CRMAPI/CRMAPI/obj/Debug/net8.0/CRMAPI.AssemblyInfo.cs
+++ API/CRMAPI/CRMAPI/obj/Debug/net8.0/CRMAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRMAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+684da08147766503ef517d4bdebc2147b37cf3bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bf8898ca0237c0ee2280d4310b52c28d7257262")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRMAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRMAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
